ad1a49f06b9ed7c23a4a2d3916d0e78d1bf9a145
Monica Fidalgo <monicafidalgo@monicas-mbp.lan>
2022-01-16 22:44:26 +0000

    remove unsed button

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import logo from "../../assets/logo.png";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import "./NavBar.scss";
 
