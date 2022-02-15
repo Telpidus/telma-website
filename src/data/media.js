b542b25cc57d6d6de72940707dfca435e943e19f
MonicaFidalgo <monica.mofi@gmail.com>
2022-02-15 22:35:42 +0000

    feat(outreach): add outreach data files

---
+++ src/data/media.js
@@ -0,0 +1,24 @@
+const media = [
+  {
+    id: 1,
+    title: "A Scientist just like me!",
+    url: "https://pstt.org.uk/resources/curriculum-materials/ASJLM",
+  },
+  {
+    id: 2,
+    title: "evALLution on the local newspaper",
+    url: "https://expressodooriente.com/cegos-experimentam-a-biodiversidade/",
+  },
+  {
+    id: 3,
+    title: "Why should anyone care about Ugandan Lianas?",
+    url: "https://treesinspace.com/2019/01/03/why-should-anyone-care-about-ugandan-lianas/",
+  },
+  {
+    id: 4,
+    title: "Fast Evolving Fish!",
+    url: "https://www.sciencedaily.com/releases/2020/04/200427125154.htm",
+  },
+];
+
+export default media;
