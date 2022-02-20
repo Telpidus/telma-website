d333c8fc715ebffc6dc3b9da131c67d7c933acbf
MonicaFidalgo <monica.mofi@gmail.com>
2022-02-20 18:32:47 +0000

    feat(research): add data for publications and collaborations

---
+++ src/data/publications.js
@@ -0,0 +1,60 @@
+const publications = [
+  {
+    id: 1,
+    authors:
+      "Laurentino TG, Xavier M, Ronco F, Pina-Martins F, Domingues I, Penha B, Dias M, de Sousa A, Carrilho T, Rodrigues L, Pinheiro C*, Rato D*, Balata D*, Ayala G*, Matos M M*, Campelo M*, Botelho R",
+    title:
+      "evALLution: making basic evolution concepts accessible to people with visual impairment through a multi-sensory tree of life",
+    url: "https://evolution-outreach.biomedcentral.com/articles/10.1186/s12052-021-00143-1",
+    date: "Evolution: education and outreach (2021) 14:5; * Equal contribution",
+    outreachDescription1: "Read about 3 lessons the blind community taught us",
+    outreachLink1:
+      "https://ssecommunityblog.org/evallution-3-lessons-the-blind-community-taught-us-about-inclusive-teaching-of-evolution/",
+    outreachDescription2: "Feature in a Portuguese news paper 🇵🇹",
+    outreachLink2:
+      "https://expressodooriente.com/cegos-experimentam-a-biodiversidade/?fbclid=IwAR3lDeZogayzlJ5xdzWbGTui7DRpnQDt2XsP71Gm1fnEbTWztML7aSjXozU",
+  },
+  {
+    id: 2,
+    authors:
+      "Laurentino TG, Moser D, Roesti M, Ammann M, Frey A, Ronco F, Kueng B, Berner D ",
+    title:
+      "Genomic release-recapture experiment in the wild indicates within-generation polygenic selection in stickleback fish",
+    url: "https://www.nature.com/articles/s41467-020-15657-3",
+    date: "Nature Communications (2020) 11, 1928",
+    outreachDescription1:
+      "Read about this article on Nature Ecology and Evolution ‘Behind the paper",
+    outreachLink1:
+      "https://ecoevocommunity.nature.com/posts/65697-chasing-selection",
+  },
+  {
+    id: 3,
+    authors: "Laurentino TG, Baur J, Usui T, Eichhorn M ",
+    title:
+      "Liana abundance and relationships to sapling and tree hosts in an East African Primary Forest",
+    url: "https://onlinelibrary.wiley.com/doi/10.1111/aje.12584",
+    date: "African journal of ecology (2019) 57(1), 130-136",
+    outreachDescription1:
+      "Read about why anyone should care about Ugandan Lianas",
+    outreachLink1:
+      "https://treesinspace.com/2019/01/03/why-should-anyone-care-about-ugandan-lianas/",
+  },
+  {
+    id: 4,
+    authors: "Laurentino TG*, Haenel Q*, Roesti M, Berner D",
+    title:
+      "Meta-analysis of chromosome-scale crossover rate variation in eukaryotes and its significance to evolutionary genomics",
+    url: "https://onlinelibrary.wiley.com/doi/10.1111/mec.14699",
+    date: "Molecular ecology (2018) 27 (11), 2477-2497",
+  },
+  {
+    id: 5,
+    authors: "Laurentino TG, Pais MP, Rosa GM ",
+    title:
+      "From a local observation to a european-wide phenomenon: Amphibian deformities in Serra da Estrela Natural Park, Portugal",
+    url: "https://ojs.herpetologica.org/index.php/bah/article/view/46/49",
+    date: "Basic and Applied Herpetology (2016) 30, 7-23 ",
+  },
+];
+
+export default publications;
