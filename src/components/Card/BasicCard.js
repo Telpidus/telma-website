80ad4f5fb7d5bf91c698b867464c16efcd6ddc43
MonicaFidalgo <monica.mofi@gmail.com>
2022-02-20 23:19:45 +0000

    feat(research): improve basic card

--- src/components/Card/BasicCard.js
+++ src/components/Card/BasicCard.js
@@ -6,19 +6,34 @@ function BasicCard(props) {
     <div className="basic-card">
       <p className="basic-card__headline">{props.date}</p>
 
-      <a href={props.url} className="basic-card__link">
+      <a
+        href={props.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="basic-card__link"
+      >
         <h3>{props.title}</h3>
       </a>
       <p className="basic-card__info">{props.author}</p>
       <div>
         {props.link1 ? (
-          <a href={props.link1} className="basic-card__link-secondary">
-            {props.subtitle1}{" "}
+          <a
+            href={props.link1}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="basic-card__link-secondary"
+          >
+            {props.subtitle1} <i className="fas fa-arrow-right"></i>
           </a>
         ) : null}
         {props.link2 ? (
-          <a href={props.link2} className="basic-card__link-secondary">
-            {props.subtitle2}{" "}
+          <a
+            href={props.link2}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="basic-card__link-secondary mt-2"
+          >
+            {props.subtitle2} <i className="fas fa-arrow-right"></i>
           </a>
         ) : null}
       </div>
