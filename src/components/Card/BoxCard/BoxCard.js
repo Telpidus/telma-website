251b80275596e1ed11b6dd24c05a4e43060d1b86
MonicaFidalgo <monica.mofi@gmail.com>
2022-02-21 16:37:27 +0000

    feat(outreach): adjust blog card and add icons and data for outreach pages

---
+++ src/components/Card/BoxCard/BoxCard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./box-card.scss";
+
+function BoxCard(props) {
+  return (
+    <a
+      href={props.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="box-card"
+    >
+      <p className="box-card__headline">{props.date}</p>
+
+      <h3 className="box-card__title">{props.title}</h3>
+
+      <p className="box-card__info">{props.description}</p>
+      <img src={props.icon} alt={props.alt} className="box-card__icon" />
+    </a>
+  );
+}
+
+export default BoxCard;
--- src/components/Outreach/Outreach.js
+++ src/components/Outreach/Outreach.js
@@ -2,9 +2,36 @@ import React from "react";
 import BannerImage from "../Banner/BannerImage";
 import Banner from "../Banner/Banner";
 import YoutubeEmbed from "../Youtube/YoutubeEmbed";
+import BlogCard from "../Blog/BlogCard";
+import BoxCard from "../Card/BoxCard/BoxCard";
+import articles from "../../data/articles";
+import mediaContent from "../../data/media";
+import { Row, Col, Container } from "react-bootstrap";
 import translatingImage from "../../assets/Home/translating.png";
 import bannerimage from "../../assets/banner_contact_hands.webp";
 import "./outreach.scss";
+
+const mediaArray = mediaContent.map((item) => (
+  <Col md={4} key={item.id}>
+    <BoxCard
+      url={item.url}
+      title={item.title}
+      description={item.description}
+      icon={item.icon}
+      alt={item.alt}
+    />
+  </Col>
+));
+
+const articlesArray = articles.map((article) => (
+  <Col md={4} className="project-card" key={article.id}>
+    <BlogCard
+      imgPath={article.image}
+      link={article.url}
+      title={article.title}
+    />
+  </Col>
+));
 
 function Outreach() {
   return (
@@ -14,7 +41,17 @@ function Outreach() {
         description="I want everyone to be able to learn about the evolution of life on our planet! "
         imgPath={translatingImage}
       />
-      <YoutubeEmbed embedId="4o3JLsXH12w?t=10" />
+      <Container>
+        <YoutubeEmbed embedId="4o3JLsXH12w?t=10" />
+        <Row>
+          <h3>Science for ALL</h3>
+        </Row>
+        <Row>{articlesArray}</Row>
+        <Row>
+          <h3>Media Features</h3>
+        </Row>
+        <Row>{mediaArray}</Row>
+      </Container>
 
       <Banner
         backgroundImage={bannerimage}
