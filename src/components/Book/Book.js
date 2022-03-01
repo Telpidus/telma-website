e8eb7ccbcd5f6f66985b86ebec73dff8f9fcd551
MonicaFidalgo <monica.mofi@gmail.com>
2022-03-01 21:56:22 +0000

    feat(books): create book summary component

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -2,19 +2,43 @@ import React from "react";
 import BannerImage from "../Banner/BannerImage";
 import Banner from "../Banner/Banner";
 import Carousel from "../Carousel/Carousel";
+import BookSummary from "./BookSummary";
+import { Row, Col, Container } from "react-bootstrap";
 import embodyingImage from "../../assets/Home/embodying.png";
 import bannerimage from "../../assets/banner_contact_hands.webp";
 import readers from "../../data/readers.js";
+import "./book.scss";
 
 function Book() {
   return (
     <div className="book-section">
       <BannerImage
-        title="BOOKS"
-        description="A love letter From the Forest to you, through me"
+        title="Writtings"
+        description={[
+          "A love letter",
+          <h3>From the Forest</h3>,
+          "to you, through me",
+        ]}
         imgPath={embodyingImage}
       />
-      <Carousel data={readers} />
+      <Container>
+        <Row>
+          <Col md={12}>
+            <p>
+              “My chest was full because I was experiencing something that I was
+              sure about, that I dedicated my life to studying, but never felt
+              that deeply before: I was but one branch in the tree of life and
+              connected to all others by ancestry. One might imagine that such a
+              realization, when truly sensed, would make you feel tiny and
+              insignificant, but no. My body and mind were filled by a new and
+              unabashed sense of belonging.”
+            </p>
+          </Col>
+        </Row>
+        <BookSummary />
+        <Carousel data={readers} />
+      </Container>
+
       <Banner
         backgroundImage={bannerimage}
         buttonLabel="Email me"
