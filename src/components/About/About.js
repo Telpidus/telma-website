4ea7a2109691b42874822af67cdc63c700e60f43
MonicaFidalgo <monica.mofi@gmail.com>
2022-03-02 22:57:22 +0000

    feat(about): add image and create content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,27 @@
-import { Container, Row } from "react-bootstrap";
 import React from "react";
+import AboutDescription from "./AboutDescription";
+import { Row, Col, Container } from "react-bootstrap";
+import treeOfLife from "../../assets/ToL_about.webp";
+import "./about.scss";
 
 function About() {
   return (
-    <Container fluid className="about-section">
-      <Container>
-        <Row>About</Row>
-      </Container>
+    <Container className="about">
+      <h2 className="about-title">Hi, I'm Telma!</h2>
+      <Row>
+        <Col md={8}>
+          <figure>
+            <img src={treeOfLife} alt="Tree of Life Illustration" />
+            <figcaption>
+              Tree of life built with the students from the Hellen Keller
+              inclusive school
+            </figcaption>
+          </figure>
+        </Col>
+        <Col md={4}>
+          <AboutDescription />
+        </Col>
+      </Row>
     </Container>
   );
 }
