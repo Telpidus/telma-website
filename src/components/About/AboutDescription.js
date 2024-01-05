4e4b1e523b98bb3d1ac75446e5902e45e64c0a81
Telpidus <telma.laurentino@gmail.com>
2024-01-05 20:14:39 +0000

    Update AboutDescription.js

--- src/components/About/AboutDescription.js
+++ src/components/About/AboutDescription.js
@@ -5,22 +5,24 @@ function About() {
     <div className="about__description">
       <p>
         <b>
-        I’m scientist, communicator and educator.
-        As an evolutionary biologist, I study how species evolve and adapt to
-        environmental change, as a writter and educator I focus on systemic 
-        socio-ecological issues and how they shape our relationship with nature!
+        I’m a scientist, communicator and educator.
+    
+        As evolutionary biologist, I study how species evolve and adapt to
+        environmental change, as writter and educator I focus on systemic 
+        socio-ecological issues and how they shape our relationship with each other and nature.
         </b>
       </p>
       <p>
-          I was born and raised in Portugal, and my research on population genomics 
-          travelled me across 5 continents, where I learned biodiversity from different 
+          I was born and raised in Portugal, and my research in evolution & genomics 
+          took me across 5 continents, where I learned biodiversity from different 
           species and within different communities. 
           Reptiles hold a special wonder-full place in my heart!   
       </p>
       <p>
         <b>
           In 2019, I founded evALLution: an education project to make evolution
-          and biodiversity accessible for, and with, people with blindness.
+          and biodiversity accessible for, and with, people with blindness. This woke up a love for multisensory education, 
+          and ideas of biodiversity knowledge as path towards belonging. 
         </b>
       </p>
       <p>
@@ -30,9 +32,9 @@ function About() {
       </p>
       <p>
         <b>
-          Currently, I'm working on designing multisensory and culturaly-responsive
-          education resources that foster complex systemic thinkig and kindle a 
-          true sense of shared ancestry with and belonging to life on Earth.
+          Currently, I'm working on designing culturaly-responsive
+          education resources that foster complex systemic thinking and kindle a 
+          true sense of shared ancestry with and belonging to all life on Earth.
         </b>
       </p>
     </div>
