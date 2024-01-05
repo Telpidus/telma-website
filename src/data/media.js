8374325cd1ce755b96e1e4dda3620d477d6765db
Telpidus <telma.laurentino@gmail.com>
2024-01-05 15:49:13 +0000

    Adds media feature card

--- src/data/media.js
+++ src/data/media.js
@@ -2,10 +2,20 @@ import liana from "../assets/media/liana-icon.png";
 import fish from "../assets/media/fish-icon.png";
 import science from "../assets/media/science-icon.png";
 import newspaper from "../assets/media/newspaper-icon.png";
+import lizard from "../assets/media/lizard-icon.png";
 
 const media = [
-  {
+ {
     id: 1,
+    title: "The Salt Basin Dunes' Lizards on the radio",
+    description:
+      "A Nature Notes podcast with fellow research Drew Dittmer for the Marfa public radio",
+    url: "https://www.marfapublicradio.org/show/nature-notes/2023-03-09/icons-of-adaptation-tracing-the-evolutionary-journey-of-west-texas-blanched-lizards",
+    icon: lizard,
+    alt: "Lizard Icon",
+  },
+  {
+    id: 2,
     title: "A Scientist just like me!",
     description:
       "Introducing children to a diverse range of scientists and people who work in science-related jobs",
@@ -14,7 +24,7 @@ const media = [
     alt: "Science Icon",
   },
   {
-    id: 2,
+    id: 3,
     title: "evALLution on the local newspaper",
     description:
       "knowledge of evolution, biodiversity and the concepts of natural history accessible to visually impaired and blind people.",
@@ -23,7 +33,7 @@ const media = [
     alt: "Newspaper Icon",
   },
   {
-    id: 3,
+    id: 4,
     title: "Why should anyone care about Ugandan Lianas?",
     description:
       "The causes of this increase in lianas are unknown, and it is also uncertain how widespread the effect might be",
@@ -32,7 +42,7 @@ const media = [
     alt: "Liana Icon",
   },
   {
-    id: 4,
+    id: 5,
     title: "Fast Evolving Fish!",
     description:
       "Researchers have identified the genetic basis of rapid adaptation using a native fish species",
