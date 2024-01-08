2842b1aebbfc219c1bcfc86226b2bd7b3a0f0cc4
Telpidus <telma.laurentino@gmail.com>
2024-01-08 14:51:17 +0000

    Update TeachingDescription.js

--- src/components/Teaching/TeachingDescription.js
+++ src/components/Teaching/TeachingDescription.js
@@ -6,43 +6,42 @@ function TeachingDescription() {
   return (
     <div className="teaching-description">
       <p>
-        When being a teacher and mentor, I believe my work is to bring my
-        acquired knowledge into the room while holding space for an inclusive,
-        diverse and engaging learning experience for all involved.
+      <b>
+       Designing multidisciplinary curriculum & learning and teaching, simultaneously, are some of my favourite activities as a scholar.
+      </b>  
       </p>
-      <p>I use three personal guiding principles:</p>
+      <p>I teach in different contexts:</p>
       <ul className="font-size-medium mt-5 mb-5">
         <li className="mb-3">
-          <i className="fa fa-book color--secondary"></i> I flow between teacher
-          and student, talker & listener.
+          <i className="fa fa-book color--secondary"></i> Formal education - University courses & invited lectures 
+           <br />
+          Evolutionary biology & Ecology & Bioinformatics
         </li>
         <li className="mb-3">
-          <i className="fa fa-heart color--secondary"></i> I hold an education
-          space that honors creativity, allows for diversity/discordance and is
-          grounded in belonging.
+          <i className="fa fa-tree color--secondary"></i> Informal education - Science outreach in schools & other education institutions
+           <br />
+          Biodiversity & Evolution
         </li>
         <li className="mb-3">
           {" "}
-          <i className="fa fa-tree color--secondary"></i> My lecture plan is
-          responsive to the learning needs and curiosities of the class
+          <i className="fa fa-heart color--secondary"></i> Depth education - Virtual and in-person community spaces
+            <br />
+          Ecosystemic awareness & Socio-Ecological issues
         </li>
       </ul>
       <p>
-        I transport the fluidity and non-duality of nature into the classroom.
-        This allows my students to bring their individual experiences, skills,
-        and stories into the classroom, so that we build and inclusive and
-        diverse learning environment together.
-        <br />
-        We collectively learn the class topic, while leanring from and with each
-        other.
+          In any context I try to hold education spaces grounded in belonging, that honor creativity and scientific rigour while allowing uncertainty, diversity and non-violent disagreement.
+          I try to make lecture plans multisensory, interactive and responsive to the learning needs & curiosities of the group.
+          <br />
+          <b>
+          When designing and teaching curriculum I apply student-centered, active, 3D learning principles focused on core ideas, crosscutting concepts, and ethically guided scientific practices.
+          <br />
+          </b>
       </p>
       <p>
-        I am interested in an education system that provides knowledge that
-        seeds environmental and social transformation, not one where the work is
-        finished with the completion of a final exam. Thus, I don't enter
-        education spaces as a builder of a course, but as an arborist that
-        adapts to the intelectual and human needs of the students and seeds the
-        future by teaching and learning.
+      <b>
+        I am interested in contributing to an equitable education system that provides multidisciplinary knowledge that supports systemic thinking at the service of inclusive and sustainable futures, for all.
+      </b>
       </p>
     </div>
   );
